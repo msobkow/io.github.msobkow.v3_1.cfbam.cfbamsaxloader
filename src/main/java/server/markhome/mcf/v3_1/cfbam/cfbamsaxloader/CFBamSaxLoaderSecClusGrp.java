001ba157f04f29f7d1550108e45581b91f7ea59c
Mark Sobkow <mark.sobkow@gmail.com>
2026-07-27 14:54:49 -0600

    2026-07-27 14:55 CST Work in progress that actually runs the CFBam code production without getting stuck in an infinite loop.

---
+++ src/main/java/server/markhome/mcf/v3_1/cfbam/cfbamsaxloader/CFBamSaxLoaderSecClusGrp.java
@@ -0,0 +1,261 @@
+
+// Description: Java 25 XML SAX Element Handler for SecClusGrp
+
+/*
+ *	server.markhome.mcf.CFBam
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal CFBam 3.1 Business Application Model
+ *	
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *	
+ *	This file is part of Mark's Code Fractal CFBam.
+ *	
+ *	Mark's Code Fractal CFBam is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU General Public License,
+ *	Version 3 or later with classpath and static linking exceptions.
+ *	
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the GPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-GPLv3-compatibly licensed code. You may not
+ *	implement an LPGLv3 "wedge" to try to bypass this restriction. That said, code which
+ *	does not rely on this library is free to specify whatever license its authors decide
+ *	to use. Mark Sobkow specifically rejects the infectious nature of the GPLv3, and
+ *	considers the mere act of including GPLv3 modules in an executable to be perfectly
+ *	reasonable given tools like modern Java's single-jar deployment options.
+ *	
+ *	Mark's Code Fractal CFBam is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFBam is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with Mark's Code Fractal CFBam.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package server.markhome.mcf.v3_1.cfbam.cfbamsaxloader;
+
+import java.math.*;
+import java.sql.*;
+import java.text.*;
+import java.time.*;
+import java.util.*;
+import org.apache.commons.codec.binary.Base64;
+import org.xml.sax.*;
+import server.markhome.mcf.v3_1.cflib.*;
+import server.markhome.mcf.v3_1.cflib.dbutil.*;
+import server.markhome.mcf.v3_1.cflib.inz.Inz;
+import server.markhome.mcf.v3_1.cflib.xml.*;
+import server.markhome.mcf.v3_1.cfsec.cfsec.*;
+import server.markhome.mcf.v3_1.cfint.cfint.*;
+import server.markhome.mcf.v3_1.cfbam.cfbam.*;
+import server.markhome.mcf.v3_1.cfsec.cfsecobj.*;
+import server.markhome.mcf.v3_1.cfint.cfintobj.*;
+import server.markhome.mcf.v3_1.cfbam.cfbamobj.*;
+
+/*
+ *	CFBamSaxLoaderSecClusGrpParse XML SAX Element Handler implementation
+ *	for SecClusGrp.
+ */
+public class CFBamSaxLoaderSecClusGrp
+	extends CFLibXmlCoreElementHandler
+{
+	public CFBamSaxLoaderSecClusGrp( CFBamSaxLoader saxLoader ) {
+		super( saxLoader );
+	}
+
+	public void startElement(
+		String		uri,
+		String		localName,
+		String		qName,
+		Attributes	attrs )
+	throws SAXException
+	{
+		final String S_ProcName = "startElement";
+		ICFBamSecClusGrpObj origBuff = null;
+		ICFBamSecClusGrpEditObj editBuff = null;
+		// Common XML Attributes
+		String attrId = null;
+		// SecClusGrp Attributes
+		// SecClusGrp References
+		ICFBamClusterObj refCluster = null;
+		ICFBamSecSysGrpObj refSysGrp = null;
+		// Attribute Extraction
+		String attrLocalName;
+		int numAttrs;
+		int idxAttr;
+		final String S_LocalName = "LocalName";
+		try {
+			assert qName.equals( "SecClusGrp" );
+
+			CFBamSaxLoader saxLoader = (CFBamSaxLoader)getParser();
+			if( saxLoader == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"getParser()" );
+			}
+
+			ICFBamSchemaObj schemaObj = saxLoader.getSchemaObj();
+			if( schemaObj == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"getParser().getSchemaObj()" );
+			}
+
+			// Instantiate an edit buffer for the parsed information
+			origBuff = (ICFBamSecClusGrpObj)schemaObj.getSecClusGrpTableObj().newInstance();
+			editBuff = (ICFBamSecClusGrpEditObj)origBuff.beginEdit();
+
+			// Extract Attributes
+			numAttrs = attrs.getLength();
+			for( idxAttr = 0; idxAttr < numAttrs; idxAttr++ ) {
+				attrLocalName = attrs.getLocalName( idxAttr );
+				if( attrLocalName.equals( "Id" ) ) {
+					if( attrId != null ) {
+						throw new CFLibUniqueIndexViolationException( getClass(),
+							S_ProcName,
+							S_LocalName,
+							attrLocalName );
+					}
+					attrId = attrs.getValue( idxAttr );
+				}
+				else if( attrLocalName.equals( "schemaLocation" ) ) {
+					// ignored
+				}
+				else {
+					throw new CFLibUnrecognizedAttributeException( getClass(),
+						S_ProcName,
+						getParser().getLocationInfo(),
+						attrLocalName );
+				}
+			}
+
+			// Ensure that required attributes have values
+
+			// Save named attributes to context
+			CFLibXmlCoreContext curContext = getParser().getCurContext();
+			curContext.putNamedValue( "Id", attrId );
+
+			// Convert string attributes to native Java types
+			// and apply the converted attributes to the editBuff.
+
+			Integer natId;
+			if( ( attrId != null ) && ( attrId.length() > 0 ) ) {
+				natId = Integer.valueOf( Integer.parseInt( attrId ) );
+			}
+			else {
+				natId = null;
+			}
+			// Get the scope/container object
+
+			CFLibXmlCoreContext parentContext = curContext.getPrevContext();
+			Object scopeObj;
+			if( parentContext != null ) {
+				scopeObj = parentContext.getNamedValue( "Object" );
+			}
+			else {
+				scopeObj = null;
+			}
+
+			// Resolve and apply required Container reference
+
+			if( scopeObj == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"scopeObj" );
+			}
+			else if( scopeObj instanceof ICFBamSecSysGrpObj ) {
+				refSysGrp = (ICFBamSecSysGrpObj) scopeObj;
+				editBuff.setRequiredContainerSysGrp( refSysGrp );
+				refCluster = (ICFBamClusterObj)editBuff.getRequiredOwnerCluster();
+			}
+			else {
+				throw new CFLibUnsupportedClassException( getClass(),
+					S_ProcName,
+					"scopeObj",
+					scopeObj,
+					"ICFBamSecSysGrpObj" );
+			}
+
+			// Resolve and apply Owner reference
+
+			if( refCluster == null ) {
+				if( scopeObj instanceof ICFBamClusterObj ) {
+					refCluster = (ICFBamClusterObj) scopeObj;
+					editBuff.setRequiredOwnerCluster( refCluster );
+				}
+				else {
+					throw new CFLibNullArgumentException( getClass(),
+						S_ProcName,
+						0,
+						"Owner<Cluster>" );
+				}
+			}
+
+			CFBamSaxLoader.LoaderBehaviourEnum loaderBehaviour = saxLoader.getSecClusGrpLoaderBehaviour();
+			ICFBamSecClusGrpEditObj editSecClusGrp = null;
+			ICFBamSecClusGrpObj origSecClusGrp = (ICFBamSecClusGrpObj)schemaObj.getSecClusGrpTableObj().readSecClusGrpByUNameIdx( refCluster.getRequiredId(),
+			refSysGrp.getRequiredName() );
+			if( origSecClusGrp == null ) {
+				editSecClusGrp = editBuff;
+			}
+			else {
+				switch( loaderBehaviour ) {
+					case Insert:
+						break;
+					case Update:
+						editSecClusGrp = (ICFBamSecClusGrpEditObj)origSecClusGrp.beginEdit();
+						break;
+					case Replace:
+						editSecClusGrp = (ICFBamSecClusGrpEditObj)origSecClusGrp.beginEdit();
+						editSecClusGrp.deleteInstance();
+						editSecClusGrp = null;
+						origSecClusGrp = null;
+						editSecClusGrp = editBuff;
+						break;
+				}
+			}
+
+			if( editSecClusGrp != null ) {
+				if( origSecClusGrp != null ) {
+					editSecClusGrp.update();
+				}
+				else {
+					origSecClusGrp = (ICFBamSecClusGrpObj)editSecClusGrp.create();
+				}
+				editSecClusGrp = null;
+			}
+
+			curContext.putNamedValue( "Object", origSecClusGrp );
+		}
+		catch( RuntimeException e ) {
+			throw new SAXException( "Near " + getParser().getLocationInfo() + ": Caught and rethrew " + e.getClass().getName() + " - " + e.getMessage(),
+				e );
+		}
+		catch( Error e ) {
+			throw new SAXException( "Near " + getParser().getLocationInfo() + ": Caught and rethrew " + e.getClass().getName() + " - " + e.getMessage() );
+		}
+	}
+
+	public void endElement(
+		String		uri,
+		String		localName,
+		String		qName )
+	throws SAXException
+	{
+	}
+}
---
+++ src/main/java/server/markhome/mcf/v3_1/cfbam/cfbamsaxloader/CFBamSaxLoaderSecSysGrp.java
@@ -0,0 +1,275 @@
+
+// Description: Java 25 XML SAX Element Handler for SecSysGrp
+
+/*
+ *	server.markhome.mcf.CFBam
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal CFBam 3.1 Business Application Model
+ *	
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *	
+ *	This file is part of Mark's Code Fractal CFBam.
+ *	
+ *	Mark's Code Fractal CFBam is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU General Public License,
+ *	Version 3 or later with classpath and static linking exceptions.
+ *	
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the GPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-GPLv3-compatibly licensed code. You may not
+ *	implement an LPGLv3 "wedge" to try to bypass this restriction. That said, code which
+ *	does not rely on this library is free to specify whatever license its authors decide
+ *	to use. Mark Sobkow specifically rejects the infectious nature of the GPLv3, and
+ *	considers the mere act of including GPLv3 modules in an executable to be perfectly
+ *	reasonable given tools like modern Java's single-jar deployment options.
+ *	
+ *	Mark's Code Fractal CFBam is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFBam is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with Mark's Code Fractal CFBam.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package server.markhome.mcf.v3_1.cfbam.cfbamsaxloader;
+
+import java.math.*;
+import java.sql.*;
+import java.text.*;
+import java.time.*;
+import java.util.*;
+import org.apache.commons.codec.binary.Base64;
+import org.xml.sax.*;
+import server.markhome.mcf.v3_1.cflib.*;
+import server.markhome.mcf.v3_1.cflib.dbutil.*;
+import server.markhome.mcf.v3_1.cflib.inz.Inz;
+import server.markhome.mcf.v3_1.cflib.xml.*;
+import server.markhome.mcf.v3_1.cfsec.cfsec.*;
+import server.markhome.mcf.v3_1.cfint.cfint.*;
+import server.markhome.mcf.v3_1.cfbam.cfbam.*;
+import server.markhome.mcf.v3_1.cfsec.cfsecobj.*;
+import server.markhome.mcf.v3_1.cfint.cfintobj.*;
+import server.markhome.mcf.v3_1.cfbam.cfbamobj.*;
+
+/*
+ *	CFBamSaxLoaderSecSysGrpParse XML SAX Element Handler implementation
+ *	for SecSysGrp.
+ */
+public class CFBamSaxLoaderSecSysGrp
+	extends CFLibXmlCoreElementHandler
+{
+	public CFBamSaxLoaderSecSysGrp( CFBamSaxLoader saxLoader ) {
+		super( saxLoader );
+	}
+
+	public void startElement(
+		String		uri,
+		String		localName,
+		String		qName,
+		Attributes	attrs )
+	throws SAXException
+	{
+		final String S_ProcName = "startElement";
+		ICFBamSecSysGrpObj origBuff = null;
+		ICFBamSecSysGrpEditObj editBuff = null;
+		// Common XML Attributes
+		String attrId = null;
+		// SecSysGrp Attributes
+		String attrName = null;
+		String attrSecLevel = null;
+		String attrImplSysRole = null;
+		// SecSysGrp References
+		// Attribute Extraction
+		String attrLocalName;
+		int numAttrs;
+		int idxAttr;
+		final String S_LocalName = "LocalName";
+		try {
+			assert qName.equals( "SecSysGrp" );
+
+			CFBamSaxLoader saxLoader = (CFBamSaxLoader)getParser();
+			if( saxLoader == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"getParser()" );
+			}
+
+			ICFBamSchemaObj schemaObj = saxLoader.getSchemaObj();
+			if( schemaObj == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"getParser().getSchemaObj()" );
+			}
+
+			// Instantiate an edit buffer for the parsed information
+			origBuff = (ICFBamSecSysGrpObj)schemaObj.getSecSysGrpTableObj().newInstance();
+			editBuff = (ICFBamSecSysGrpEditObj)origBuff.beginEdit();
+
+			// Extract Attributes
+			numAttrs = attrs.getLength();
+			for( idxAttr = 0; idxAttr < numAttrs; idxAttr++ ) {
+				attrLocalName = attrs.getLocalName( idxAttr );
+				if( attrLocalName.equals( "Id" ) ) {
+					if( attrId != null ) {
+						throw new CFLibUniqueIndexViolationException( getClass(),
+							S_ProcName,
+							S_LocalName,
+							attrLocalName );
+					}
+					attrId = attrs.getValue( idxAttr );
+				}
+				else if( attrLocalName.equals( "Name" ) ) {
+					if( attrName != null ) {
+						throw new CFLibUniqueIndexViolationException( getClass(),
+							S_ProcName,
+							S_LocalName,
+							attrLocalName );
+					}
+					attrName = attrs.getValue( idxAttr );
+				}
+				else if( attrLocalName.equals( "SecLevel" ) ) {
+					if( attrSecLevel != null ) {
+						throw new CFLibUniqueIndexViolationException( getClass(),
+							S_ProcName,
+							S_LocalName,
+							attrLocalName );
+					}
+					attrSecLevel = attrs.getValue( idxAttr );
+				}
+				else if( attrLocalName.equals( "ImplSysRole" ) ) {
+					if( attrImplSysRole != null ) {
+						throw new CFLibUniqueIndexViolationException( getClass(),
+							S_ProcName,
+							S_LocalName,
+							attrLocalName );
+					}
+					attrImplSysRole = attrs.getValue( idxAttr );
+				}
+				else if( attrLocalName.equals( "schemaLocation" ) ) {
+					// ignored
+				}
+				else {
+					throw new CFLibUnrecognizedAttributeException( getClass(),
+						S_ProcName,
+						getParser().getLocationInfo(),
+						attrLocalName );
+				}
+			}
+
+			// Ensure that required attributes have values
+			if( attrName == null ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"Name" );
+			}
+			if( ( attrSecLevel == null ) || ( attrSecLevel.length() <= 0 ) ) {
+				throw new CFLibNullArgumentException( getClass(),
+					S_ProcName,
+					0,
+					"SecLevel" );
+			}
+
+			// Save named attributes to context
+			CFLibXmlCoreContext curContext = getParser().getCurContext();
+			curContext.putNamedValue( "Id", attrId );
+			curContext.putNamedValue( "Name", attrName );
+			curContext.putNamedValue( "SecLevel", attrSecLevel );
+			curContext.putNamedValue( "ImplSysRole", attrImplSysRole );
+
+			// Convert string attributes to native Java types
+			// and apply the converted attributes to the editBuff.
+
+			Integer natId;
+			if( ( attrId != null ) && ( attrId.length() > 0 ) ) {
+				natId = Integer.valueOf( Integer.parseInt( attrId ) );
+			}
+			else {
+				natId = null;
+			}
+			String natName = attrName;
+			editBuff.setRequiredName( natName );
+
+			ICFSecSchema.SecLevelEnum natSecLevel = ICFSecSchema.parseSecLevelEnum( attrSecLevel );
+			editBuff.setRequiredSecLevel( natSecLevel );
+
+			// Get the scope/container object
+
+			CFLibXmlCoreContext parentContext = curContext.getPrevContext();
+			Object scopeObj;
+			if( parentContext != null ) {
+				scopeObj = parentContext.getNamedValue( "Object" );
+			}
+			else {
+				scopeObj = null;
+			}
+
+			CFBamSaxLoader.LoaderBehaviourEnum loaderBehaviour = saxLoader.getSecSysGrpLoaderBehaviour();
+			ICFBamSecSysGrpEditObj editSecSysGrp = null;
+			ICFBamSecSysGrpObj origSecSysGrp = (ICFBamSecSysGrpObj)schemaObj.getSecSysGrpTableObj().readSecSysGrpByUNameIdx( editBuff.getRequiredName() );
+			if( origSecSysGrp == null ) {
+				editSecSysGrp = editBuff;
+			}
+			else {
+				switch( loaderBehaviour ) {
+					case Insert:
+						break;
+					case Update:
+						editSecSysGrp = (ICFBamSecSysGrpEditObj)origSecSysGrp.beginEdit();
+						editSecSysGrp.setRequiredName( editBuff.getRequiredName() );
+						editSecSysGrp.setRequiredSecLevel( editBuff.getRequiredSecLevel() );
+						break;
+					case Replace:
+						editSecSysGrp = (ICFBamSecSysGrpEditObj)origSecSysGrp.beginEdit();
+						editSecSysGrp.deleteInstance();
+						editSecSysGrp = null;
+						origSecSysGrp = null;
+						editSecSysGrp = editBuff;
+						break;
+				}
+			}
+
+			if( editSecSysGrp != null ) {
+				if( origSecSysGrp != null ) {
+					editSecSysGrp.update();
+				}
+				else {
+					origSecSysGrp = (ICFBamSecSysGrpObj)editSecSysGrp.create();
+				}
+				editSecSysGrp = null;
+			}
+
+			curContext.putNamedValue( "Object", origSecSysGrp );
+		}
+		catch( RuntimeException e ) {
+			throw new SAXException( "Near " + getParser().getLocationInfo() + ": Caught and rethrew " + e.getClass().getName() + " - " + e.getMessage(),
+				e );
+		}
+		catch( Error e ) {
+			throw new SAXException( "Near " + getParser().getLocationInfo() + ": Caught and rethrew " + e.getClass().getName() + " - " + e.getMessage() );
+		}
+	}
+
+	public void endElement(
+		String		uri,
+		String		localName,
+		String		qName )
+	throws SAXException
+	{
+	}
+}
